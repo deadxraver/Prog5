f9c2d031c84ebc4b682624b5969fa9e7b545863a
Даниил <160046659+deadxraver@users.noreply.github.com>
2025-10-28 08:42:53 +0300

    Delete javadoc directory

--- javadoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"commandInterfacePack"},{"l":"commandsPack"},{"l":"elementsPack"},{"l":"exceptionsPack"},{"l":"mainPack"},{"l":"parserPack"}];updateSearchResults();
